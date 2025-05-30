6e7fc44587b39d6c7e75caa6f9102e87c6da6601
Xicofrancio <franciscodpfsousa@gmail.com>
2025-05-30 23:25:55 +0100

    remove unecessary files

--- tests/run_backup.ps1
+++
@@ -1,3 +0,0 @@
-$backupScript = "run job=MinIOBackup yes"
-docker cp .\run_backup.txt app-bacula-server-1:/tmp/run_backup.txt
-docker exec app-bacula-server-1 /usr/sbin/bconsole -c "/etc/bacula/bconsole.conf" -u root -n -d 200
